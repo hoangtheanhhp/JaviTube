145bc9850377e5f5bcb3a4edd401c02d0d9b236e
Anh <anhht@haposoft.com>
2018-12-11 22:08:59 +0700

    fix

--- resources/views/song/index.blade.php
+++ resources/views/song/index.blade.php
@@ -3,10 +3,11 @@
 @include('includes.header')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <div class="container">
-    <div class="col-md-6">
-      <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/{{ $song->youtube_id }}?rel=0&autoplay=1&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-      </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/{{ $song->youtube_id }}?rel=0&autoplay=1&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
         <div>
           @if (Auth::check())
             @if (Auth::user()->liked($song->id))
@@ -19,18 +20,9 @@
           @endif
 
           <span>{{ $song->be_liked->count() }} likes</span>
-    </div>
-    <div>
-      <a id="like">like</a>
-      <a id="unlike">unlike</a>
-      <h1>
-        <div id="liked_number">
-          {{\DB::table('like')->where('song_id',$song->id)->count()}} liked!!!
         </div>
-        
-      </h1>
-    </div>
-    <div class="col-md-6">
+      </div>
+      <div class="col-md-6">
         <ul class="nav nav-tabs">
             <li><a href="#originalLyric">Original</a></li>
             <li><a href="#translation">Translation</a></li>
@@ -41,7 +33,12 @@
             <div role="tabpanel" class="tab-pane fade" id="Events"></div>
           </div>
         </div>
+      </div>
+      <div class="col-md-6">
+      </div>
     </div>
+
+
 </div>
       <script>
         $(document).ready(function(){
